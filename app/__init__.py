3b915e4a0729600119fac1ff1d7ca7c30f4150f7
Heitor65 <multheitor@gmail.com>
2026-06-19 18:39:10 -0300

    fix: importação main_bp

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,6 @@ from flask import Flask
 
 def create_app():
     app=Flask(__name__)
-    from routes.main import main_bp
+    from app.routes.main import main_bp
     app.register_blueprint(main_bp)
     return app
