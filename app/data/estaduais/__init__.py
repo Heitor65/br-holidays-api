e993f1a530de1e73d99ea5a547f694de8a10628d
Heitor65 <multheitor@gmail.com>
2026-06-26 13:08:59 -0300

    __init__ dos estaduais

--- app/data/estaduais/__init__.py
+++ app/data/estaduais/__init__.py
@@ -1,9 +1,9 @@
-from centro_oeste.df import feriados_df
-from centro_oeste.ms import feriados_ms
-from sudeste.sp import feriados_sp
-from sudeste.rj import feriados_rj
-from sul.rs import feriados_rs
-from sul.sc import feriados_sc
+from app.data.estaduais.centro_oeste.df import feriados_df
+from app.data.estaduais.centro_oeste.ms import feriados_ms
+from app.data.estaduais.sudeste.sp import feriados_sp
+from app.data.estaduais.sudeste.rj import feriados_rj
+from app.data.estaduais.sul.rs import feriados_rs
+from app.data.estaduais.sul.sc import feriados_sc
 
 
 FERIADOS_POR_ESTADO = {
