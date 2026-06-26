7b1e13768db4e14deb9c870f08c5f67b2875bdf5
Heitor65 <multheitor@gmail.com>
2026-06-26 11:35:01 -0300

    adição do model_dump() que esqueci

--- app/data/estaduais/sul/rs.py
+++ app/data/estaduais/sul/rs.py
@@ -3,5 +3,5 @@ from app.models.DayMonth import DayMonth
 
 
 feriados_rs = [
-    Feriado(name="Dia do Gaúcho", date=DayMonth(day=20, month=9), description="Celebra a Data Magna gaúcha. A data homenageia o início da Revolução Farroupilha em 1835, o mais longo conflito civil da história do Brasil.")
+    Feriado(name="Dia do Gaúcho", date=DayMonth(day=20, month=9), description="Celebra a Data Magna gaúcha. A data homenageia o início da Revolução Farroupilha em 1835, o mais longo conflito civil da história do Brasil.").model_dump()
 ]
--- app/data/estaduais/sul/sc.py
+++ app/data/estaduais/sul/sc.py
@@ -3,5 +3,5 @@ from app.models.DayMonth import DayMonth
 
 
 feriados_sc = [
-    Feriado(name="Data Magna do Estado de Santa Catarina", date=DayMonth(day=11, month=8), description="Celebra a criação da capitania de Santa Catarina (sua emancipação política em 1738).") # Se cair em dia util o feriado é transferido para domingo
+    Feriado(name="Data Magna do Estado de Santa Catarina", date=DayMonth(day=11, month=8), description="Celebra a criação da capitania de Santa Catarina (sua emancipação política em 1738).").model_dump()  # Se cair em dia util o feriado é transferido para domingo
 ]
