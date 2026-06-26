bed3dafca48bb19d395f6a0e6dd8b60403032977
Heitor65 <multheitor@gmail.com>
2026-06-26 13:09:22 -0300

    endpoint get para estaduais

--- app/routes/main.py
+++ app/routes/main.py
@@ -1,9 +1,17 @@
 from flask import jsonify, request, Blueprint
 from pydantic import ValidationError
 from app.data.nacionais import feriados_nacionais
+from app.data.estaduais import FERIADOS_POR_ESTADO
 
 main_bp = Blueprint('main_bp', __name__)
 
 @main_bp.route('/nacional', methods=['GET'])
 def get_feriados_nacionais():
     return jsonify(feriados_nacionais), 200
+
+@main_bp.route('/estaduais/<uf>', methods=['GET'])
+def get_feriados_estadual(uf):
+    feriados = FERIADOS_POR_ESTADO.get(uf.upper())
+    if not feriados:
+        return jsonify({"error": "UF não encontrada"}), 404
+    return jsonify(feriados), 200
