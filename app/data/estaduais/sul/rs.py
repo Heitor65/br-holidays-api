8fa5177165cd7e6450c2f13590ae17239386d127
Heitor65 <multheitor@gmail.com>
2026-06-26 11:25:44 -0300

    feruadis estaduais do sul

---
+++ app/data/estaduais/sul/rs.py
@@ -0,0 +1,7 @@
+from app.models.Feriados import Feriado
+from app.models.DayMonth import DayMonth
+
+
+feriados_rs = [
+    Feriado(name="Dia do Gaúcho", date=DayMonth(day=20, month=9), description="Celebra a Data Magna gaúcha. A data homenageia o início da Revolução Farroupilha em 1835, o mais longo conflito civil da história do Brasil.")
+]
---
+++ app/data/estaduais/sul/sc.py
@@ -0,0 +1,7 @@
+from app.models.Feriados import Feriado
+from app.models.DayMonth import DayMonth
+
+
+feriados_sc = [
+    Feriado(name="Data Magna do Estado de Santa Catarina", date=DayMonth(day=11, month=8), description="Celebra a criação da capitania de Santa Catarina (sua emancipação política em 1738).") # Se cair em dia util o feriado é transferido para domingo
+]
